c1c6e9ee09d8923a0e0fa80069bc0e55a258246b
navedwz <navedwz@gmail.com>
2025-03-13 19:04:15 +0600

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+export default function Document({ Html, Head, Main, NextScript }) {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
