2f40da3071eeb72d353a11a39b97dee789153e05
navedwz <navedwz@gmail.com>
2025-03-13 15:36:10 +0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Stock Market Website</h1>
+      <p>This website will display stock information similar to AmarStock.</p>
+    </div>
+  );
+}
